52cefa31d842551d627ebf16fef0b8016100aa24
unknown <muhammadrafas3010@gmail.com>
2025-10-22 16:37:15 +0700

    u

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets, cityList } from "../assets/assets";
 
 const Hero = () => {
+
+    const [pickupLocation, setPickupLocation] = useState()
+
   return (
     <div className="h-screen flex flex-col items-center justify-center gap-14 bg-light text-center">
       <h1 className="text-4xl md:text-5xl font-semibold">
@@ -10,16 +13,30 @@ const Hero = () => {
 
       <form className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]'>
         <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8'>
-          <div>
-            <select required>
+          <div className='flex flex-col items-start gap-2'>
+            <select required value = {pickupLocation} onChange={(e)=>setPickupLocation(e.target.value)}>
               <option value="">Pickup Location</option>
               {cityList.map((city)=> <option key={city} value={city}>{city}
                 </option>
               )}
             </select>
-            <p className='px-1 text-sm text-gray-500'>Please select location</p>
+            <p className='px-1 text-sm text-gray-500'>{pickupLocation ? 
+            pickupLocation : 'Please select location'}</p>
           </div>
+          <div className='flex flex-col items-start gap-2'>
+              <label htmlFor='pickup-date'>pick-up Date</label>
+              <input type="date" id="pickup-date" min={new Date().toISOString().split('T')[0]} className='text-sm text-gray-500' required/>
+          </div>
+          <div className='flex flex-col items-start gap-2'>
+              <label htmlFor='return-date'>return-date</label>
+              <input type="date" id="return-date"  className='text-sm text-gray-500' required/>
+          </div>
+          <button className='flex item-center Justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounde-full cursor-pointer'>
+            <img src="assets.search_icon" alt="search" className='brighness-300'/>
+            Search
+          </button>
         </div>
+
       </form>
 
       <img src={assets.main_car} alt="car" className="max-h-74" />
