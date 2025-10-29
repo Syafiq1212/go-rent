e160054ba153eefd194f5beb82d6200e2f6c3dff
unknown <tikbelgisyafiq@gmail.com>
2025-10-29 15:10:18 +0700

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,9 @@ import Home from "./pages/Home";
 import Cars from "./pages/Cars";
 import MyBookings from "./pages/MyBookings";
 import Footer from "./components/Footer";
+import Layout from "./pages/owner/Layout";
+import AddCar from "./pages/owner/AddCar";
+import ManageBookings from "./pages/owner/ManageBookings";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -20,6 +23,12 @@ const App = () => {
         <Route path="/car-details/:id" element={<CarDetails />} />
         <Route path="/cars" element={<Cars />} />
         <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/owner" element={<Layout />}>
+          <Route index element={<Dashboard />}/>
+          <Route path="add-car" element={<AddCar />}/>
+          <Route path="manage-car" element={<ManageCar />}/>
+          <Route path="manage-bookings" element={<ManageBookings />}/>
+        </Route>
       </Routes>
 
       {!isOwnerPath && <Footer />}
---
+++ client/src/components/owner/Sidebar.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { dummyUserData } from '../../assets/assets'
+import { useLocation } from 'react-router-dom';
+
+const Sidebar = () => {
+
+    const user = dummyUserData;
+    const location = useLocation()
+    const [image, setImage] = useState('')
+
+    const updateImage = async ()=>{
+        user.image = URL.createObjectURL(image)
+        setImage
+    }
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Sidebar
